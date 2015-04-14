cd73ff49a6331ffbb0d5eed6e18501c91ab5ce9b
MariotoA <malexrc7@gmail.com>
2015-04-14 19:34:03 +0200

    Refactorización de estilo

---
+++ src/main/java/org/iis/prdoublelinkedlist/DoubleLinkedListException.java
@@ -0,0 +1,22 @@
+package org.iis.prdoublelinkedlist;
+
+public class DoubleLinkedListException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+
+    public DoubleLinkedListException() {
+        super();
+    }
+
+    public DoubleLinkedListException(String msg) {
+        super(msg);
+    }
+
+    public DoubleLinkedListException(int i) {
+        super(Integer.toString(i));
+    }
+
+}
